9559bb19734eedaf172e4d14599fd71db9d6f90f
guibaumer <117869143+guibaumer@users.noreply.github.com>
2024-04-02 14:48:31 -0300

    push

--- src/components/LoginForm/Form.tsx
+++ src/components/LoginForm/Form.tsx
@@ -46,7 +46,7 @@ export default function Form() {
                     } else {
                         login();
                         toast.success(resText);
-                        router.push('/');
+                        // router.push('/');
                     }
 
                 } catch (err) {
