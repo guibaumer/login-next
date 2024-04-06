fa504849fe262fc6c815489781d2f8fa163fc358
guibaumer <117869143+guibaumer@users.noreply.github.com>
2024-04-06 14:56:54 -0300

    warning

--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -10,10 +10,14 @@ export default function HomePage() {
       <Header />
       <h2>HOME</h2>
       <div className={styles.container}>
-        <div className={styles.info}>Esta aplicação foi criada com Next.js.
-          Ela está conectada com uma API criada por mim com Node.js, que por sua vez está
+        <div className={styles.info}>
+          Esta aplicação foi criada com Next.js.
+          Ela está conectada com uma API criada por mim com Node.js e Express, que por sua vez está
           ligada à um banco de dados criado com PostgreSQL.
-          Se trata de um sistema de login que usa sessions para a autenticação.
+          Se trata de um sistema de login que usa sessions para a autenticação. <strong>
+            Pelo fato dos serviços que usei para fazer deploy serem gratuitos, pode haver um delay
+            de uns 30s na primeira conexão com a api/db.
+          </strong>
         </div>
       </div>
       <Footer />
